809ad0447ecbdc73e38b783b348319de9bacdee0
vishal modanwal <vk0764394@gmail.com>
2024-12-23 16:10:20 +0530

    update configration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server :{
-    proxy : {
-      "/api" : "http://localhost:8000"
-    }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://blog-ai-1-us79.onrender.com', // Backend URL
+        changeOrigin: true, // Ensures the Host header matches the target
+        secure: true,       // Use this if your backend uses HTTPS
+      },
+    },
   },
   plugins: [react()],
-})
+});
