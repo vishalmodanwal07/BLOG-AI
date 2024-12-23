86bc8cf1bf56c0a2dcd068e75a4e1b9617db3ff3
vishal modanwal <vk0764394@gmail.com>
2024-12-23 08:41:17 +0530

    configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    proxy: {
+      '/api':' https://blog-ai-1-us79.onrender.com' , 
+    },
+  },
 })
