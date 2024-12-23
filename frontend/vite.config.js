7e330256a5745685140a60b5b2fd9c496e3a1c87
vishal modanwal <vk0764394@gmail.com>
2024-12-23 09:32:33 +0530

    Readme file  updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api':' https://blog-ai-1-us79.onrender.com' , 
-    },
+  server :{
+    proxy : {
+      "/api" : "http://localhost:8000"
+    }
   },
+  plugins: [react()],
 })
